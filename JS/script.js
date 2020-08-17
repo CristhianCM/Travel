455a9e7b60e8c39eaf9f26763b5400dcab54e825
Cristhia Carrion <cristhiancarrioncornejo@hotmail.com>
2020-08-17 12:08:00 -0500

    Travel

--- JS/script.js
+++ JS/script.js
@@ -1,41 +1,41 @@
 const enlaces = document.getElementsByClassName("enlaces")[0];
 const toogle = document.getElementsByClassName("toogle")[0];
 const menutoogle = document.getElementById("menutoogle");
-let abierto = false
+let abierto = false;
 
 const toogleMenu = () => {
   enlaces.classList.toggle("enlaces2");
   enlaces.style.transition = "transform 0.5s ease-in-out";
-}
+};
 
 toogle.addEventListener("click", function () {
   toogleMenu();
   if (document.querySelector(".enlaces.enlaces2")) {
-    abierto=true;
+    abierto = true;
   } else {
-    abierto=false;
+    abierto = false;
   }
-})
+});
 
-window.addEventListener("click", function(e){
-  this.console.log(e.target)
-  if(abierto){
-    if(e.target !== menutoogle){
-        toogleMenu();
-        abierto=false;
+window.addEventListener("click", function (e) {
+  this.console.log(e.target);
+  if (abierto) {
+    if (e.target !== menutoogle) {
+      toogleMenu();
+      abierto = false;
     }
   }
-})
+});
 
-window.addEventListener("resize", function(){
-  if(screen.width > 927){
-    if(abierto){
+window.addEventListener("resize", function () {
+  if (screen.width > 927) {
+    if (abierto) {
       toogleMenu();
       enlaces.style.transition = "none";
-      abierto=false;
+      abierto = false;
     }
   }
-})
+});
 
 // -----------------------LigthMode---------
 const btnSwitch = document.querySelector("#switch");
@@ -44,7 +44,6 @@ btnSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   btnSwitch.classList.toggle("active");
 });
-
 
 // window.addEventListener("click", function(){
 //   toogleMenu();
